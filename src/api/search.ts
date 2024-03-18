435caac6c1561967b26d5ad386278125a2fa64dc
skyeome <tjru7230@naver.com>
2024-03-18 17:18:50 +0900

    fix: env 설정 수정2

--- src/api/search.ts
+++ src/api/search.ts
@@ -25,8 +25,12 @@ export const bookSearch = axios.create({
   baseURL: import.meta.env.PROD ? BOOK_SEARCH_URL : "/api",
   headers: {
     "Content-type": "application/json; charset=UTF-8",
-    "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
-    "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
+    "X-Naver-Client-Id": import.meta.env.DEV
+      ? process.env.NAVER_CLIENT_ID
+      : undefined,
+    "X-Naver-Client-Secret": import.meta.env.DEV
+      ? process.env.NAVER_CLIENT_SECRET
+      : undefined,
   },
 });
 
