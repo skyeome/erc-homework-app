a62c0e9297d33aaf63407c5827e983d4387136a6
skyeome <tjru7230@naver.com>
2024-04-11 23:11:30 +0900

    fix: kebabToPascal 제거

--- src/pages/admin/homework/level.tsx
+++ src/pages/admin/homework/level.tsx
@@ -7,33 +7,15 @@ import WeeklyLevelHomework from "@/components/admin/homework/level/WeeklyLevelHo
 import WeeklySetDateAndCategory from "@/components/admin/homework/level/WeeklySetDateAndCategory";
 import { HomeworkTypes } from "@/components/admin/dashboard/notification/index.types";
 
-function kebabToPascal(inputString?: string) {
-  if (inputString === undefined) return "";
-  // 문자열을 '-'를 기준으로 나눠 배열로 저장
-  const parts = inputString.split("-");
-
-  // 첫 번째 부분의 첫 글자를 대문자로 변경
-  parts[0] = parts[0].charAt(0).toUpperCase() + parts[0].slice(1);
-
-  // 나머지 부분들을 순회하면서 각각의 첫 글자를 대문자로 변경
-  for (let i = 1; i < parts.length; i++) {
-    parts[i] = parts[i].charAt(0).toUpperCase() + parts[i].slice(1);
-  }
-
-  // 배열의 각 요소를 공백으로 이어 붙여 하나의 문자열로 만듦
-  return parts.join(" ");
-}
-
 function AdminHomeworkLevel() {
   const { levelName } = useParams();
-  const title = kebabToPascal(levelName);
   const [category, setCategory] = useState<HomeworkTypes>("record");
   const date = useAppSelector((state) => state.date.value);
 
   return (
     <div>
       <Typography variant="h4" fontWeight={700} p={2}>
-        {title}
+        {levelName}
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} md={5}>
